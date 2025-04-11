714b6fab2230daef5a1070e4c0a3316295d6b7a1
Evgenii Kurchenko <daskorwaflype01@gmail.com>
2025-04-11 22:22:16 +0300

    Добавлен скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+//Создание карточки
+function createCard(obj, removeCard) {
+  const template = document.querySelector('#card-template').content;
+  const cardElement = template.cloneNode(true);
+  const card = cardElement.querySelector('.card'); 
 
-// @todo: DOM узлы
+  card.querySelector('.card__image').src = obj.link;
+  card.querySelector('.card__image').alt = obj.name;
+  card.querySelector('.card__title').textContent = obj.name;
+  
+  card.querySelector('.card__delete-button').addEventListener('click', () => {
+    removeCard(card);
+  });
 
-// @todo: Функция создания карточки
+  return cardElement;
+}
 
-// @todo: Функция удаления карточки
+//Добавление карточек в DOM
+initialCards.forEach((obj) => {
+  document.querySelector('.places__list').appendChild(createCard(obj, removeCard))
+})
 
-// @todo: Вывести карточки на страницу
+//Удаление карточки
+function removeCard(element) {
+    if (element && element.remove) {
+        element.remove()
+    } else {
+        if (element && element.parentNode) {
+            element.parentNode.removeChild(element)
+        }
+    }
+}
+
+
