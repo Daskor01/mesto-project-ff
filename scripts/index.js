90625ab05d9e94e3c870a2f6fe201604cad811b0
Evgenii Kurchenko <daskorwaflype01@gmail.com>
2025-04-12 10:43:25 +0300

    Обновлен скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,29 @@
+//Получаем элементы из DOM
+const template = document.querySelector('#card-template')
+const cardList = document.querySelector('.places__list')
+
 //Создание карточки
 function createCard(obj, removeCard) {
-  const template = document.querySelector('#card-template').content;
-  const cardElement = template.cloneNode(true);
-  const card = cardElement.querySelector('.card'); 
+  const cardElement = template.content.cloneNode(true)
+  const card = cardElement.querySelector('.card')
+  const cardImage = card.querySelector('.card__image')
+  const cardTitle = card.querySelector('.card__title')
+  const deleteButton = card.querySelector('.card__delete-button')
 
-  card.querySelector('.card__image').src = obj.link;
-  card.querySelector('.card__image').alt = obj.name;
-  card.querySelector('.card__title').textContent = obj.name;
-  
-  card.querySelector('.card__delete-button').addEventListener('click', () => {
-    removeCard(card);
-  });
+  cardImage.alt = obj.name
+  cardImage.src = obj.link
+  cardTitle.textContent = obj.name
 
-  return cardElement;
+  deleteButton.addEventListener('click', () => {
+    removeCard(card)
+  })
+
+  return cardElement
 }
 
 //Добавление карточек в DOM
 initialCards.forEach((obj) => {
-  document.querySelector('.places__list').appendChild(createCard(obj, removeCard))
+  cardList.appendChild(createCard(obj, removeCard))
 })
 
 //Удаление карточки
@@ -25,10 +31,6 @@ function removeCard(element) {
     if (element && element.remove) {
         element.remove()
     } else {
-        if (element && element.parentNode) {
-            element.parentNode.removeChild(element)
-        }
+      element.closest('.card').remove()
     }
 }
-
-
